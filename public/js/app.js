0ceae72d4c57a083e9cd8ce586be482555b82b31
Jon Borgonia <jon@gomagames.com>
2018-06-19 18:37:54 -0700

    :lipstick: styled for great good

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,8 @@
 (document => {
   const uploadForm = document.querySelector('#upload_form');
   const resultContainer = document.querySelector('#result');
+  const fileField = uploadForm.querySelector('#image_upload');
+  const fileNameContainer = uploadForm.querySelector('.file-name');
 
   uploadForm.addEventListener('submit', e => {
     e.preventDefault();
@@ -8,7 +10,6 @@
     resultContainer.innerHTML = ''; // clear last submission
 
     const body = new FormData();
-    const fileField = uploadForm.querySelector('#image_upload');
 
     body.append(fileField.getAttribute('name'), fileField.files[0]);
 
@@ -16,18 +17,24 @@
       method: 'POST',
       body
     })
-    .then(res => res.json())
-    .catch(error => console.error('Error:', error))
-    .then(res => {
-      const imageEl = document.createElement('img');
-      imageEl.src = res.publicPath;
-      resultContainer.appendChild(imageEl);
+      .then(res => res.json())
+      .catch(error => console.error('Error:', error))
+      .then(res => {
+        const imageEl = document.createElement('img');
+        imageEl.src = res.publicPath;
+        resultContainer.appendChild(imageEl);
 
-      const messageEl = document.createElement('p');
-      messageEl.innerText = `${res.originalName} successfully uploaded to ${res.publicPath}`;
-      resultContainer.appendChild(messageEl);
-      console.debug('Done');
-    });
+        const messageEl = document.createElement('p');
+        messageEl.innerText = `${res.originalName} successfully uploaded to ${res.publicPath}`;
+        resultContainer.appendChild(messageEl);
+        console.debug('Done');
+      });
+  });
+
+  // below is only for pretty form field
+  fileField.addEventListener('change', () => {
+    if (fileField.files.length > 0)
+      fileNameContainer.innerHTML = fileField.files[0].name;
   });
 
 })(document);
